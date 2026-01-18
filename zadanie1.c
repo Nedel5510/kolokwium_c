577d5a2cb7fdea2182d4ed03d0a95d76de9594ea
Nedel5510 <n.edel.579@studms.ug.edu.pl>
2026-01-18 09:59:27 +0100

    Improve input validation in main function
    Added validation for non-positive starting numbers.

--- zadanie1.c
+++ zadanie1.c
@@ -7,11 +7,16 @@ int number_simulation(int start) {
     while (n >= 10) {
         int ostatnia_cyfra = n % 10;
 
-        if (ostatnia_cyfra >= 0 && ostatnia_cyfra <= 3) {
+        if (ostatnia_cyfra == 0) {
+            n--;  
+        } 
+        else if (ostatnia_cyfra >= 1 && ostatnia_cyfra <= 3) {
             n = n + (ostatnia_cyfra * 2);
-        } else if (ostatnia_cyfra >= 4 && ostatnia_cyfra <= 6) {
+        } 
+        else if (ostatnia_cyfra >= 4 && ostatnia_cyfra <= 6) {
             n = n - 3;
-        } else { // cyfry 7-9
+        } 
+        else { 
             n = n / 2;
         }
 
@@ -27,9 +32,17 @@ int main() {
     printf("Podaj liczbe startowa: ");
     scanf("%d", &start);
 
+    if (start <= 0) {
+        printf("Podano niepoprawna liczbe. Podaj liczbe dodatnia.\n");
+        return 1;
+    }
+
     int wynik = number_simulation(start);
 
     printf("Liczba krokow: %d\n", wynik);
 
     return 0;
 }
+
+
+
